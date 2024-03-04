eee7b4b67abfb627cc6c1eb4cfe7eb886333f65f
AryanB01 <aryanbhattarai000@gmail.com>
2024-03-04 09:06:45 -0500

    new blog code by prof

---
+++ app.js
@@ -0,0 +1,84 @@
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
+
+let index = require('./controllers/index');
+let users = require('./controllers/users');
+let auth = require('./controllers/auth');
+
+// custom controllers
+let posts = require('./controllers/posts');
+
+// custom imports
+let mongoose = require('mongoose');
+let dotenv = require('dotenv');
+let passport = require('passport');
+let session = require('express-session');
+
+let app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// if app is not in production mode, connect to .env file for global vars
+// these global vars set in the Render dashboard in production
+if (process.env.NODE_ENV != 'production') {
+  require('dotenv').config();
+}
+
+// mongodb connection BEFORE controllers included
+mongoose.connect(process.env.CONNECTION_STRING)
+.then((res) => { console.log('Connected to MongoDB') })
+.catch(() => { console.log('MongoDB connection failed') });
+
+// passport local auth config.  MUST be BEFORE controllers that use it 
+// 1. configure session support
+app.use(session({
+  secret: 'string-here-for-now-but-should-be-in-ENV',
+  resave: true,
+  saveUninitialized: false
+}));
+
+// 2. enable passport w/sessions
+app.use(passport.initialize());
+app.use(passport.session());
+
+// 3. link passport to our User model & use local strategy by default
+let User = require('./models/user');
+passport.use(User.createStrategy());
+
+// 4. enable session reads / writes for passport users
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use('/', index);
+app.use('/users', users);
+app.use('/posts', posts);
+app.use('/auth', auth);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ controllers/posts.js
@@ -0,0 +1,89 @@
+let express = require('express');
+let router = express.Router();
+
+// Post model for CRUD
+let Post = require('../models/post');
+
+/* GET: /posts => show main blog page */
+router.get('/', async(req, res) => {
+    // use model to fetch all Post data from MongoDB
+    let posts = await Post.find().sort({ 'date': -1 });
+    // console.log(posts[0]);
+
+    // load view and pass all the json data to it for display
+    res.render('posts/index', {
+        title: 'Our Lastest Random Thoughts',
+        posts: posts
+    });
+});
+
+/* GET: /posts/details/abc123 => display selected post */
+router.get('/details/:_id', async (req, res) => {
+    //console.log(req.params._id);
+    let post = await Post.findById(req.params._id);
+
+    //console.log(post);
+    res.render('posts/details', {
+        title: 'Blog Post',
+        post: post
+    });
+});
+
+/* GET: /posts/create => display new post form */
+router.get('/create', (req, res) => {
+    res.render('posts/create', {
+        title: 'Create New Post'
+    });
+});
+
+/* POST: /posts/create => process form submission to save new blog post */
+router.post('/create', async (req, res) => {
+    // use mongoose model to save new post to db
+    await Post.create(req.body);
+
+    // reload the posts/index view to see the updated blog list
+    res.redirect('/posts');
+});
+
+/* GET: /posts/delete/abc123 => remove selected doc & redirect */
+router.get('/delete/:_id', async (req, res) => {
+    // delete selected doc based on _id in url param
+    await Post.findByIdAndDelete(req.params._id);
+
+    // redirect
+    res.redirect('/posts');
+});
+
+/* GET: /posts/edit/abc123 => display selected doc in form */
+router.get('/edit/:_id', async (req, res) => {
+    // get selected doc from db
+    let post = await Post.findById(req.params._id);
+
+    // load view & pass data
+    res.render('posts/edit', {
+        title: 'Edit Post',
+        post: post
+    });
+});
+
+/* POST: /posts/edit/abc123 => updated doc from form submission */
+router.post('/edit/:_id', async (req, res) => {
+    // update doc
+    await Post.findByIdAndUpdate(req.params._id, req.body);
+
+    // redirect
+    res.redirect('/posts');
+});
+
+router.post('/search', async(req, res) => {
+    let posts = await Post.find({ $text: { $search: req.body.keyword }});
+    console.log(posts);
+    res.render('posts/search', { 
+        title: 'Search Results', 
+        posts: posts,
+        keyword: req.body.keyword
+    });
+});
+
+// make public
+module.exports = router;
